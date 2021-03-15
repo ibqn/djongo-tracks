5d7d2bebd714a768ea6b3c87b8e2e23759d09716
ibqn <evgen.ibqn@gmail.com>
2021-03-16 00:27:51 +0100

    set up mongo database and migrations

--- tracks/models.py
+++ tracks/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# from django.db import models
+from djongo import models
 
 # Create your models here.
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
