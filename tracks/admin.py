32d724e1b43052a292c92db2c05de1d5d590425c
ibqn <evgen.ibqn@gmail.com>
2021-03-17 17:21:12 +0100

    add track table to django admin

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from tracks.models import Track
+
 # Register your models here.
+
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "posted_by")
