17135f550d467e6d65373d7fe26fbcb4969d56a6
ibqn <evgen.ibqn@gmail.com>
2021-04-10 00:08:59 +0200

    add default auto field  settings for tracks app

--- tracks/apps.py
+++ tracks/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class TracksConfig(AppConfig):
+    # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+    default_auto_field = "django.db.models.AutoField"
+
     name = "tracks"
